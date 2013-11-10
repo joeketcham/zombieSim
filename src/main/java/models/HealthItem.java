a569b9522fced4fa28af6a9cb43856ebcadae137
Joe Ketcham <joeketcham@gmail.com>
2013-11-09 21:01:08 -0500

    implementation for items

--- src/main/java/models/HealthItem.java
+++ src/main/java/models/HealthItem.java
@@ -1,5 +1,31 @@
 package models;
 
-public class HealthItem
+public class HealthItem extends InanimateObject
 {
+	private String description;
+	private int bonus;
+	public HealthItem(){
+		description = "health";
+		bonus = 0;
+	}
+	public HealthItem(String args[]){
+		//desc, bonus, skill, weight
+		description = args[0];
+		bonus = Integer.parseInt(args[1]);
+		super.setSkill(Integer.parseInt(args[2]));
+		super.setWeight(Integer.parseInt(args[3]));
+	}
+	public void setDescription(String description){ 
+		this.description = description;
+	}
+	public String getDescription(){
+		return description;
+	}
+	
+	public void setValue(int bonus){
+		this.bonus = bonus;
+	}
+	public int useItem(){
+		return bonus;
+	}
 }
